c3da92929268176d97b3df9e9d70ad3e9e4517b3
gaurav thalpati <gauravthalpati@gmail.com>
2022-04-25 15:35:11 +0530

    With STS identity caller

--- pythonS3write.py
+++ pythonS3write.py
@@ -2,15 +2,18 @@ import boto3
 
 #Creating Session With Boto3.
 session = boto3.Session(
-aws_access_key_id='<KEY ID>',
-aws_secret_access_key='<ACCESS KEY>'
+aws_access_key_id='',
+aws_secret_access_key='/'
 )
+
+sts = boto3.client('sts')
+print(sts.get_caller_identity())
 
 #Creating S3 Resource From the Session.
 s3 = session.resource('s3')
-
 txt_data = b'SAMPLE DATA'
 
+#write to S3
 object = s3.Object('<bucket-name>', '<folder-name>/cust_data3.csv')
 
 result = object.put(Body=txt_data)
